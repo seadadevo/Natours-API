d7b8f5a1f5511e9e0e710209d3ca241489534dd1
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-12 14:23:44 +0300

    Add Path to edit a tour  by id  /api/v1/tours/:id

--- app.js
+++ app.js
@@ -56,6 +56,32 @@ app.get('/api/v1/tours/:id', (req, res) => {
     }) 
 })
 
+//! patch data
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Tour not Found'
+        })
+    }
+
+    Object.assign(tour, req.body)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+    })
+
+})
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App runing on port ${port}...`);
