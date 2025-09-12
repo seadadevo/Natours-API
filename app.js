ababf23d42d6afb34ca614532d146571df9c5500
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-12 12:40:41 +0300

    Add POST /api/v1/tours endpoint to Add new tour

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+//! To prepate the middleware
+app.use(express.json())
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)) 
+
+
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -14,9 +19,21 @@ app.get('/api/v1/tours', (req, res) => {
     }) 
 })
 
-
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint')
+// ! Starting post 
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId} , req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+   
 })
 
 
