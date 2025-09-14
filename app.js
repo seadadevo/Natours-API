8e22803ddb3f6aeb1509dad1fc69ee0166540526
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-14 17:17:23 +0300

    1- Using Environement Variables
    2- create config.env file to put the evnironemrn type and port then using it to server and app file
    
    create condition if the mode is developement then apply morgan logger

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes')
@@ -7,9 +6,11 @@ const userRouter = require('./routes/userRoutes')
 
 
 //! 1) MIDDLEWARE
-app.use(morgan('dev'));
+if(process.env.NODE_ENV ==='development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./app')
-
-const port = 3000;
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' });
+// console.log(process.env.USER)
+console.log(process.env.NODE_ENV);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
 });
