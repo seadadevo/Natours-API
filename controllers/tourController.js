bbd88e51707ae1f9c1c93e61f2098e71d32636cc
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-14 14:31:26 +0300

    Make a middleware for send an error if the name and price are not entered

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,16 @@ exports.checkID = (req, res, next, val) => {
   next()
 }
 
+exports.checkBody = (req, res, next) => {
+  if(!req.body.name || !req.body.price ) {
+    return res.status(400).json({
+      status: 'failt',
+      message: 'Missing name or price'
+    })
+  }
+  next()
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -41,7 +51,7 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  console.log(req.body);
+
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -88,4 +98,3 @@ exports.deleteTour = (req, res) => {
     data: null,
   });
 };
-
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
-const {getAllTours, createTour, getTour, updateTour, deleteTour, checkID} = require('../controllers/tourController')
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkID,
+  checkBody,
+} = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
-tourRouter.param('id', checkID)
+tourRouter.param('id', checkID);
 
-
-tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(checkBody, createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = tourRouter;
