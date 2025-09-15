3a73211301f0a06dda62b468770bdc4b7849efc1
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-15 19:59:01 +0300

    1-using express.static to handle with html css/ icons-images file
    handling with eslint and adding the rules for it
    
     handling with the envireonment valiables and adding a condition for the appliying of morgan middleware if it was in developemnt mode
    
    install mogooos and connect with and link it with the atlas mongopdb

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const userRouter = require('./routes/userRoutes')
 
 
 //! 1) MIDDLEWARE
-if(process.env.NODE_ENV ==='development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-}
+};
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
-})
+});
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next()
-})
+});
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const app = require('./app')
-const dotenv = require('dotenv')
+const mongoose = require(`mongoose`);
+const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
-// console.log(process.env.USER)
-console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>' , process.env.DATABASE_PASSWORD )
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => {
+  // console.log(con.connections)
+  console.log('DB Connection Successful!')
+})
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
