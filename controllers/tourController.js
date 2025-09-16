ac6da38f969b3555ac9639c871a8bbe33d86addd
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-16 19:30:13 +0300

    applying Tour.find(), Tour.findById(req.params.id) and Tour create to get all tours and get tour by id and post new tour in db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,34 +1,64 @@
-const Tour = require('./../models/tourModel')
+const Tour = require("../models/tourModel")
 
-exports.checkBody = (req, res, next) => {
-  if(!req.body.name || !req.body.price ) {
-    return res.status(400).json({
-      status: 'failt',
-      message: 'Missing name or price'
+
+exports.getAllTours =async (req, res) => {
+  try {
+    const tours = await Tour.find()
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+       status: 'fail',
+     message: err
     })
   }
-  next()
-}
-
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-  });
 }; 
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    // Tour.findOne({_id: req.params.id})
+    
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    })
+
+  } catch (err) {
+    res.status(400).json({
+       status: 'fail',
+     message: err
+    })
+  }
+ 
  
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour({})
+  // newTour.save()
+try {
+  const newTour = await Tour.create(req.body)
+
    res.status(201).json({
         status: 'success',
+        data: {
+          tour: newTour
+        }
       });
+  
+} catch (err) {
+  res.status(400).json({
+    status: 'fail',
+    message: 'Invalid data sent'
+  })
+}
 
 };
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,14 +6,13 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  checkBody,
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
 // tourRouter.param('id', checkID);
 
-tourRouter.route('/').get(getAllTours).post(checkBody, createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = tourRouter;
