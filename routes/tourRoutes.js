2fc2ef676415d91603addd089797ca484741d644
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-18 05:12:47 +0300

    apply aliasing in api

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,12 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours 
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
-// tourRouter.param('id', checkID);
+tourRouter.route('/top-5-cheap').get(aliasTopTours , getAllTours)
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
