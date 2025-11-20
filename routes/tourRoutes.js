ed486c0a3bec90db9f55d1839d33a31d77ae54a8
ahmed magdy <ahmedmagdee04@gmail.com>
2025-11-20 17:41:44 +0200

    feat: implement monthly plan aggregation pipeline (unwind startDates, filter by year, group by month)

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,12 +6,16 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  aliasTopTours 
+  aliasTopTours,
+  getTourStats ,
+  getMonthlyPlan
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
 tourRouter.route('/top-5-cheap').get(aliasTopTours , getAllTours)
+tourRouter.route('/tour-stats').get(getTourStats)
+tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
