92845bb71204b8a1461346f6ef9713df1ab19864
ahmed magdy <ahmedmagdee04@gmail.com>
2025-11-26 02:32:36 +0200

    create new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const {getAllUsers, createUser, getUser, updateUser, deleteUser} = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
 const userRouter = express.Router()
+
+userRouter.post('/signup', authController.signup)
 
 userRouter.route('/').get(getAllUsers).post(createUser)
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
